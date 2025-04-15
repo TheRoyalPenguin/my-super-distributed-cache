fbda4966b583fd88da18449e435dc58963271150
Penguin <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-04-15 17:32:17 +0300

    Добавил поддержку докера, переписал Client под сетевое взаимодействие

---
+++ DistributedCache/Client/DistributedCacheClient.cs
@@ -0,0 +1,44 @@
+﻿using Node.DTO;
+using System.Net.Http.Json;
+
+namespace Client;
+
+public class DistributedCacheClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly Uri _clusterManagerUrl;
+
+    public DistributedCacheClient(HttpClient httpClient, Uri clusterManagerUrl)
+    {
+        _httpClient = httpClient;
+        _clusterManagerUrl = new Uri(clusterManagerUrl.ToString().EndsWith("/") ? clusterManagerUrl.ToString() : clusterManagerUrl.ToString() + "/");
+    }
+
+    public async Task<T> GetAsync<T>(string key)
+    {
+        var response = await _httpClient.GetAsync(_clusterManagerUrl + $"api/cluster/cache/" + Uri.EscapeDataString(key));
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? ttl = null)
+    {
+        var dto = new CacheItemDto
+        {
+            Value = value,
+            TTL = ttl
+        };
+
+        var response = await _httpClient.PutAsJsonAsync(_clusterManagerUrl + "api/cluster/cache/" + Uri.EscapeDataString(key), dto);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception();
+        }
+    }
+}
--- SuperDistributedCache/Common/ICacheNode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common;
-
-public interface ICacheNode
-{
-    T Get<T>(string key);
-    void Set<T>(string key, T value, TimeSpan? ttl);
-    void Remove(string key);
-}
