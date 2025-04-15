95159b3d6bbd74450266ba15faedf3d648ae6779
Penguin <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-04-15 17:45:16 +0300

    Реализовал базовый ClusterManager (переписал под сеть)

---
+++ DistributedCache/ClusterManager/Controllers/ClusterController.cs
@@ -0,0 +1,96 @@
+﻿using ClusterManager.DTO;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Node.DTO;
+
+namespace ClusterManager.Controllers;
+
+[Route("api/cluster")]
+[ApiController]
+public class ClusterController : ControllerBase
+{
+    private readonly List<Uri> _nodes = new();
+    private readonly object _lock = new();
+    private readonly HttpClient _httpClient;
+    public ClusterController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [HttpPost("register")]
+    public IActionResult RegisterNode([FromBody] NodeRegistrationRequest request)
+    {
+        lock (_lock)
+        {
+            var nodeUri = new Uri(request.Url);
+            if (!_nodes.Contains(nodeUri))
+            {
+                _nodes.Add(nodeUri);
+            }
+        }
+
+        return Ok();
+    }
+
+    [HttpGet("cache/{key}")]
+    public async Task<IActionResult> GetCacheItem(string key)
+    {
+        try
+        {
+            var nodeUrl = GetNodeForItemKey(key);
+            string baseUrl = nodeUrl.ToString().EndsWith("/") ? nodeUrl.ToString() : nodeUrl.ToString() + "/";
+            var requestUri = new Uri(baseUrl + "api/cache/" + Uri.EscapeDataString(key));
+
+            var response = await _httpClient.GetAsync(requestUri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPut("cache/{key}")]
+    public async Task<IActionResult> SetCacheItem(string key, [FromBody] CacheItemDto item)
+    {
+        try
+        {
+            var nodeUrl = GetNodeForItemKey(key);
+            string baseUrl = nodeUrl.ToString().EndsWith("/") ? nodeUrl.ToString() : nodeUrl.ToString() + "/";
+            var requestUri = new Uri(baseUrl + "api/cache/" + Uri.EscapeDataString(key));
+
+            var response = await _httpClient.PutAsJsonAsync(requestUri, item);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    private ActionResult<Uri> GetNodeForItemKey(string key)
+    {
+        if (_nodes.Count == 0) 
+            return NotFound();
+
+        int hash = key.GetHashCode();
+        int index = Math.Abs(hash) % _nodes.Count;
+        return _nodes[index];
+    }
+}
---
+++ DistributedCache/ClusterManager/DTO/CacheItemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Node.DTO;
+
+public class CacheItemDto
+{
+    public object Value { get; set; }
+    public TimeSpan? TTL { get; set; }
+}
