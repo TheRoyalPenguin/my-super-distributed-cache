f26c435366414d33159312ab46b2779786bd0b5e
Penguin <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-05-06 17:52:12 +0300

    Обновление Result.cs
    Добавление поддержки статус кода

--- DistributedCache/ClusterManager/Result.cs
+++ DistributedCache/ClusterManager/Result.cs
@@ -4,21 +4,23 @@ public class Result<T>
 {
     public T? Data { get; }
     public string? Error { get; }
+    public int StatusCode { get; }
     public bool IsSuccess { get; }
 
-    public Result(T? data, string? error, bool isSuccess)
+    public Result(T? data, string? error, bool isSuccess, int statusCode)
     {
         Data = data;
         Error = error;
         IsSuccess = isSuccess;
+        StatusCode = statusCode;
     }
 
-    public static Result<T> Ok(T data)
+    public static Result<T> Ok(T data, int statusCode)
     {
-        return new Result<T>(data, null, true);
+        return new Result<T>(data, null, true, statusCode);
     }
-    public static Result<T> Fail(string error)
+    public static Result<T> Fail(string error, int statusCode)
     {
-        return new Result<T>(default, error, false);
+        return new Result<T>(default, error, false, statusCode);
     }
 }
