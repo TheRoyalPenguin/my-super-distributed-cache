6d0eeee7e541a2a5dd7d632c70c79d3f93677c45
Penguin <amasharafutdinov@kpfu.ru>
2025-05-06 13:02:33 +0300

    Создание NodeConfigurationDTO.cs

---
+++ DistributedCache/ClusterManager/DTO/NodeConfigurationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ClusterManager.DTO;
+
+public class NodeConfigurationDTO
+{
+    public string Image { get; set; }
+    public string ContainerName { get; set; }
+    public Dictionary<string, string> EnvironmentVariables { get; set; }
+    public Dictionary<string, string> PortBindings { get; set; }
+}
