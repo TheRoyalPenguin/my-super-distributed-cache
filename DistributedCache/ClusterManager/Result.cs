054c12a078bd1aad86c7323387b39ca8d3c4a11a
Penguin <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-05-06 16:58:21 +0300

    Добавление ClusterManager\Result.cs

---
+++ DistributedCache/ClusterManager/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace ClusterManager;
+
+public class Result<T>
+{
+    public T? Data { get; }
+    public string? Error { get; }
+    public bool IsSuccess { get; }
+
+    public Result(T? data, string? error, bool isSuccess)
+    {
+        Data = data;
+        Error = error;
+        IsSuccess = isSuccess;
+    }
+
+    public static Result<T> Ok(T data)
+    {
+        return new Result<T>(data, null, true);
+    }
+    public static Result<T> Fail(string error)
+    {
+        return new Result<T>(default, error, false);
+    }
+}
